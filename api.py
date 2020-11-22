33245a1ae7f05c3d206b6eac0c3ab57fa237a761
avantari <nikhil.k@avantari.org>
2020-11-23 00:48:34 +0530

    final merge

--- api.py
+++ api.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, render_template, send_from_directory, Response
 import requests
-import pyodbc
 import pymongo
 import json
 
@@ -13,17 +12,51 @@ seekers = mydb["seekers"] # seekers consists of job seekers info
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-@app.route('/resume_parser', methods=['GET'])
-def resume_parser():
-    # takes a pdf file as input and returns a json file
-    return 'a'
+# @app.route('/resume_parser', methods=['GET'])
+# def resume_parser():
+#     # takes a pdf file as input and returns a json file
+#     return 'a'
+
+
+
+CVDetails = {'name':'John', 'education':'Bachelor of Science in Computer Science', 'skills':'Python, Hadoop,Mapreduce, AWS', 'Phone Number': '+1 404 124 3214', 'experience': 'Software Engineering Intern, CNN Corporation: \n Built a data pipeline to routinely collect data from Twitter, LexisNexis and Facebook APIs and store them in a Hadoop file cluster via Apache Kafka then migrated codes to AWS. \n Deployed a CNN deep neural network to classify relevant tweets with high accuracy. \nApplied various Topic Modeling techniques, Sentiment Analysis and other descriptive statistics on the extracted data and visualized them using Tableau.'}
+
+@app.route('/details-form/', methods=['GET', 'POST'])
+def formsubmission():
+    return render_template('student_profile_form.html')
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload/', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            print('No file attached in request')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            print('No file selected')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            return CVDetails
+            # pdfFileObj = request.files['file']                 
+            # pdfReader = PyPDF2.PdfFileReader(pdfFileObj)  
+            # pageObj = pdfReader.getPage(0)  
+            # print(pageObj.extractText()) 
+            # pdfFileObj.close()
+            # filename = secure_filename(file.filename)
+        else:
+            print('Upload a pdf file only') # TODO: Send this message to UI
+    return render_template('student_resume_upload.html')
+
+
+
 
 @app.route('/jd_parser', methods=['POST'])
 def jd_parser():
-    print('1')
-    skills = ['machine learning', 'python', 'c++','java','tensorflow','aurdino','keras','deep learning','ios','android','html','css','javascript','aws','azure','powerbi']
+    skills = ['machine learning', 'python', 'ux', 'ui', 'web development', 'bigdata', 'c++','java','tensorflow','aurdino','keras','deep learning','ios','android','html','css','javascript','aws','azure','powerbi']
     jd = request.data.decode('utf-8')
-    print(jd)
 
     skills_in_jd = []
 
@@ -36,11 +69,12 @@ def jd_parser():
     table_entries = []
     for application in applications:
         mydict = {}
-        score = 0
+        score = 100
         candidate_skills = application['skills'].split(',')
         for skill in skills_in_jd:
-            if(skill in candidate_skills):
-                score += 1
+            if(skill not in candidate_skills):
+                print(skill, score)
+                score -= 10
         mydict['score'] = score
         mydict['ph_no'] = application['phone']
         mydict['skills'] = application['skills']
--- web_app.py
+++ web_app.py
@@ -6,22 +6,69 @@ Created on Sat Nov 21 15:25:08 2020
 @author: nikhil
 """
 
-from flask import Flask, request, render_template, send_from_directory
+from flask import Flask, request, render_template, send_from_directory, redirect, url_for
 import requests
 import json
+import pymongo
+uri = "mongodb://hackgsu:moqcCeQKiGDZVqYkqchqXtFcDMzxWD3p9MBbIZj2F3GaLaeSAQqu487LW6TCZYLGfuKOFxNl68EKyEmrCGu9fg==@hackgsu.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@hackgsu@"
+client = pymongo.MongoClient(uri)
+mydb = client["hackgsu"]
+users = mydb["users"]
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-api_base = 'http://825fff06fe98.ngrok.io'
+api_base = 'https://ec10824514cf.ngrok.io'
 
 @app.route("/")
 def homepage():
     return render_template('index.html')
 
+@app.route("/", methods=['POST'])
+def homepage_submit():
+    return redirect(url_for('loginpage'))
+
 @app.route("/login")
 def loginpage():
     return render_template('login.html')
+
+@app.route("/student_resume_upload")
+def student_resume_upload():
+    return render_template('student_resume_upload.html')
+
+
+@app.route("/student_resume_upload", methods=['POST'])
+def student_resume_upload_submit():
+    return redirect(url_for('student_profile_form')) 
+
+@app.route("/student_profile_form")
+def student_profile_form():
+    return render_template('student_profile_form.html')
+
+@app.route("/student_profile_form", methods=['POST','GET'])
+def student_profile_form_submit():
+    return render_template('student_submitted.html')
+
+@app.route("/login", methods=['POST'])
+def loginpage_post():
+    username = request.form['username']
+    print(username)
+    x = users.find_one({"name":username})
+
+    if(x['role'] == 'seeker'):
+        return redirect(url_for('student_resume_upload')) 
+    else:
+        return redirect(url_for('input'))
+
+
+@app.route("/submit-profile/")
+def userprofile():
+    return render_template('student_profile_form.html')
+
+@app.route("/formsubmitted/")
+def usersubmission():
+    return render_template('student_submitted.html')
+
 
 @app.route("/jd")
 def input():
