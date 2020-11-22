5d958240017fb29c6913d8ebd8250cccf79038b9
avantari <nikhil.k@avantari.org>
2020-11-22 09:51:10 +0530

    Employer side

--- api.py
+++ api.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, render_template, send_from_directory
+from flask import Flask, request, render_template, send_from_directory, Response
 import requests
 import pyodbc
 import pymongo
+import json
 
 # mongodb setup
 uri = "mongodb://hackgsu:moqcCeQKiGDZVqYkqchqXtFcDMzxWD3p9MBbIZj2F3GaLaeSAQqu487LW6TCZYLGfuKOFxNl68EKyEmrCGu9fg==@hackgsu.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@hackgsu@"
@@ -12,11 +13,17 @@ seekers = mydb["seekers"] # seekers consists of job seekers info
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
+@app.route('/resume_parser', methods=['GET'])
+def resume_parser():
+    # takes a pdf file as input and returns a json file
+    return 'a'
 
-@app.route('/jd_parser', methods=['GET'])
-def home():
+@app.route('/jd_parser', methods=['POST'])
+def jd_parser():
+    print('1')
     skills = ['machine learning', 'python', 'c++','java','tensorflow','aurdino','keras','deep learning','ios','android','html','css','javascript','aws','azure','powerbi']
-    jd = request.form['jd'].lower()
+    jd = request.data.decode('utf-8')
+    print(jd)
 
     skills_in_jd = []
 
@@ -44,7 +51,7 @@ def home():
 
     sorted_entries = sorted(table_entries, key = lambda i: i['score'],reverse=True)
 
-    return "sorted_entries"
+    return Response(response=json.dumps(sorted_entries), status=200, mimetype='application/json') 
 
 
 if __name__ == "__main__":
--- web_app.py
+++ web_app.py
@@ -8,9 +8,12 @@ Created on Sat Nov 21 15:25:08 2020
 
 from flask import Flask, request, render_template, send_from_directory
 import requests
+import json
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+api_base = 'http://825fff06fe98.ngrok.io'
 
 @app.route("/")
 def homepage():
@@ -20,6 +23,25 @@ def homepage():
 def loginpage():
     return render_template('login.html')
 
+@app.route("/jd")
+def input():
+    return render_template('employer_jd.html')
+
+
+@app.route("/jd", methods=['POST'])
+def form_post(): 
+    data = request.form['data'].encode('utf-8')
+    data = data.replace(b'\n',b'')
+    reply = requests.post(api_base + "/jd_parser", data=data, timeout = 1000).content.decode("utf-8")
+    print(reply)
+    reply = json.loads(reply)
+    scores = [r['score'] for r in reply]
+    ph_nos = [r['ph_no'] for r in reply]
+    skills = [r['skills'] for r in reply]
+    education = [r['education'] for r in reply]
+    work_exp = [r['work_exp'] for r in reply]
+    return render_template('employer_table.html',mimetype='text/plain' , len = len(scores), scores = scores, ph_nos = ph_nos, skills = skills, education = education, work_exp = work_exp  )
+    
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=False, port=5001)
